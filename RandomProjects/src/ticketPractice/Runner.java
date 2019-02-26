2bca031659cfd9957ee430f2c671b67249d67a33
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-02-26 18:51:31 -0500

    added comments

--- RandomProjects/src/ticketPractice/Runner.java
+++ RandomProjects/src/ticketPractice/Runner.java
@@ -1,4 +1,13 @@
 package ticketPractice;
+/**
+ * This is the Runner class for the ticket package. It uses the Ticket
+ * and Transactions classes to do whatever you wish. These are practice
+ * codes for a future project I plan to do which includes building a
+ * program to use at bfoods. 
+ * 
+ * @author fabri
+ * @date 26 February 2019
+ */
 
 import java.util.Scanner;
 public class Runner {
@@ -12,6 +21,7 @@ public class Runner {
 	public static void main(String[] args) {
 		numOfTickets = askNum();
 		Transactions t = new Transactions(numOfTickets);
+		//calculates the total price
 		double totalAmount = 0;
 		for(int i = 0; i < t.ticketList.length;i++) {
 			totalAmount += t.ticketList[i].getPrice();
