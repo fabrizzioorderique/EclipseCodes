2d2f5031e77d86eefbcd59efe343797d07e55ef0
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-02-26 15:49:32 -0500

    finished most of transaction class

--- RandomProjects/src/RandProjectsPackage/Transactions.java
+++ RandomProjects/src/RandProjectsPackage/Transactions.java
@@ -1,5 +1,17 @@
 package RandProjectsPackage;
 
 public class Transactions {
-
+	private int numOfTickets;
+	Ticket[] ticketList;
+	
+	public Transactions(int numOfTickets) {
+		this.numOfTickets = numOfTickets;
+		ticketList = new Ticket[this.numOfTickets];
+		fillTicketList();
+	}
+	private void fillTicketList() {
+		for(int i = 0; i < numOfTickets;i++) {
+			ticketList[i] = new Ticket("n/a",i,i*10);
+		}
+	}
 }
