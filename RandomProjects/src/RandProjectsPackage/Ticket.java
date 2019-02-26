affb5197b89552db8835bfab750cbe80c4df118b
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-02-26 15:41:42 -0500

    added a transaction class and finished getters

--- RandomProjects/src/RandProjectsPackage/Ticket.java
+++ RandomProjects/src/RandProjectsPackage/Ticket.java
@@ -1,5 +1,23 @@
 package RandProjectsPackage;
 
 public class Ticket {
-
+	private String myRow;
+	private int mySeat;
+	private double myPrice;
+	
+	public Ticket(String row, int seat, double price) {
+		myRow = row;
+		mySeat = seat;
+		myPrice = price;
+	}
+	
+	public String getRow() {
+		return myRow;
+	}
+	public int getSeat() {
+		return mySeat;
+	}
+	public double getPrice() {
+		return myPrice;
+	}
 }
