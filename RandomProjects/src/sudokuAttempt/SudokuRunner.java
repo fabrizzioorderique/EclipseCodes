74e17668f7b06b6b0efe0a9ff5482aed8867d215
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-02-27 18:52:23 -0500

    added a bunch of methods

--- RandomProjects/src/sudokuAttempt/SudokuRunner.java
+++ RandomProjects/src/sudokuAttempt/SudokuRunner.java
@@ -14,6 +14,40 @@ package sudokuAttempt;
  * 2) rows and cols must contained diff nums
  * 3) 3x3 boxes must contain diff nums
  */
+import javax.swing.JOptionPane;
 public class SudokuRunner {
+	//class variables
+	private static int[][] myTable;
+	
+	//method for printing the table at a current time
+	private static void printTable(int[][] table) {
+		for(int row = 0; row < table.length; row++) {
+			for(int col = 0; col < table[row].length; col++) {
+				System.out.print(table[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	//user inputs each individual table value
+	private static void userFillTable() {
+		JOptionPane.showMessageDialog(null,"Instructions: fill in the table from left to right, top to bottom.\nType exit if you want to leave");
+		for(int row = 0; row < myTable.length; row++) {
+			for(int col = 0; col < myTable[row].length; col++) {
+				String input = JOptionPane.showInputDialog("Value at row "+(row+1)+" col "+ (col +1)+":");
+				myTable[row][col] = Integer.parseInt(input);
+			}
+		}
+	}
+	
+	//main method
+	public static void main(String[] args) {
+		myTable = new int[9][9];
+		printTable(myTable);
+		userFillTable();
+		printTable(myTable);
+		//trying to make a method for initializing table elements
+		
+	}
 	
 }
