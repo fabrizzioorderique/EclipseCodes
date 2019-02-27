e42ae6ea25f4ef65d518b899998ed20720a65d2a
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-02-27 13:44:10 -0500

    added sudoku classes to new randProj

---
+++ RandomProjects/src/sudokuAttempt/SudokuRunner.java
@@ -0,0 +1,19 @@
+package sudokuAttempt;
+/**
+ * This code is an attempt to make a sudoku solver
+ * that takes in puzzles and solves them
+ * @author fabri
+ * @date 27 February 2019
+ * 
+ * Program Requirements:
+ * 1) takes in user input to actually have a puzzle to solve
+ * 2) solves it
+ *
+ * Game Requirements:
+ * 1) numbers 1 - 9 must fill a 9x9 grid
+ * 2) rows and cols must contained diff nums
+ * 3) 3x3 boxes must contain diff nums
+ */
+public class SudokuRunner {
+	
+}
