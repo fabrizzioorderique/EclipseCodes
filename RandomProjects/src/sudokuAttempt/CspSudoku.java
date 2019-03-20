fd8d9b5ab8fb287511825307c5a54205d4abf6b8
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-03-20 09:22:02 -0400

    updated boolean functions

--- RandomProjects/src/sudokuAttempt/CspSudoku.java
+++ RandomProjects/src/sudokuAttempt/CspSudoku.java
@@ -9,10 +9,10 @@ package sudokuAttempt;
  * @date 14 March 2019
  */
 public class CspSudoku {
-	//This is the GRID that is actually going to get solved:
-	private static int[][] grid = new int[9][9];
+	//This is the board that is actually going to get solved:
+	private static int[][] board = new int[9][9];
 	
-	//This is just a stored table that can be set to grid
+	//This is just a stored table that can be set to board
 	private static int[][] testTable = new int[][]{
 		{1,0,6,0,0,2,3,0,0},
 		{0,5,0,0,0,6,0,9,1},
@@ -39,38 +39,38 @@ public class CspSudoku {
 	private static void buildBoard(int[][] table) {
 		for(int row = 0; row < 9; row++) {
 			for(int col = 0; col < 9; col++) {
-				grid[row][col] = table[row][col];
+				board[row][col] = table[row][col];
 			}
 		}
 	}
 	
 	//Checks is a number in the a row
-	private static boolean isInRow(int row, int num) {
-		for (int i = 0; i < grid[row].length; i++) {
-			if(grid[row][i] == num) {
-				return true;
+	private static boolean isNotInRow(int num, int row) {
+		for (int i = 0; i < board[row].length; i++) {
+			if(board[row][i] == num) {
+				return false;
 			}
 		}
-		return false;
+		return true;
 	}
 	
 	//checks if a number is in a column
-	private static boolean isInCol(int col, int num) {
-		for (int i = 0; i < grid.length; i++) {
-			if(grid[i][col] == num) {
-				return true;
+	private static boolean isNotInCol(int num, int col) {
+		for (int i = 0; i < board.length; i++) {
+			if(board[i][col] == num) {
+				return false;
 			}
 		}
-		return false;
+		return true;
 	}
 	
-	//checks if number is in its 3x3 subGrid
-	private static boolean isInSubgrid(int row, int col, int num) {
-		int r = row - (row%3);
-		int c = col - (col%3);
-		for (int subRow = r; subRow < r + 3; subRow++) {
-			for (int subCol = c; subCol < c + 3; subCol++) {
-				if(grid[subRow][subCol] == num) {
+	//checks if number is in its 3x3 subboard
+	private static boolean isNotInSubBoard(int num, int row, int col) {
+		int tempR =  row - (row%3);
+		int tempC = col - (col%3);
+		for (int subRow = tempR; subRow < tempR + 3; subRow++) {
+			for (int subCol = tempC; subCol < tempC + 3; subCol++) {
+				if(board[subRow][subCol] == num) {
 					return true;
 				}
 			}
@@ -80,6 +80,6 @@ public class CspSudoku {
 	
 	//checks to see if all conditions have been met
 	private static boolean thisNumWorks(int row, int col, int num) {
-		return (!isInRow(row, num) && !isInCol(col, num) && !isInSubgrid(row, col, num));
+		return (isNotInRow(num, row) && isNotInCol(num, col) && isNotInSubBoard(num, row, col));
 	}
 }
