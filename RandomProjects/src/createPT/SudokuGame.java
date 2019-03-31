9a3d15c79665ec57c1bd49c68b55d4ec35c710be
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-03-30 20:57:18 -0400

    added the makeRandBoard method for randomness

--- RandomProjects/src/createPT/SudokuGame.java
+++ RandomProjects/src/createPT/SudokuGame.java
@@ -15,14 +15,15 @@ package createPT;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class SudokuGame extends JFrame implements ActionListener{
 
 	//actual board that will be solved
 	private static int[][] board = new int[9][9];
 	
-	//Easy Board
-	private int[][] easyTable = new int[][]{
+	//Easy Boards
+	private int[][] easyTable1 = new int[][]{
 		{0,0,5,6,1,4,8,9,2},
 		{8,4,2,9,7,3,5,6,1},
 		{9,6,1,2,8,5,3,7,4},
@@ -33,8 +34,19 @@ public class SudokuGame extends JFrame implements ActionListener{
 		{6,9,4,7,3,8,2,1,5},
 		{3,2,8,5,6,1,7,4,9},
 	};
-	//Medium Board
-	private int[][] mediumTable = new int[][]{
+	private int[][] easyTable2 = new int[][] {
+		{9,5,7,6,1,3,2,8,4},
+		{4,8,3,2,5,7,1,9,6},
+		{6,1,2,8,4,9,5,3,7},
+		{1,7,8,3,6,4,9,5,2},
+		{5,2,4,9,7,1,3,6,8},
+		{3,6,9,5,2,8,7,4,1},
+		{8,4,5,7,9,2,6,1,3},
+		{2,9,1,4,3,6,8,7,5},
+		{0,0,6,1,8,5,4,2,9},
+	};
+	//Medium Boards
+	private int[][] mediumTable1 = new int[][]{
 		{1,0,6,0,0,2,3,0,0},
 		{0,5,0,0,0,6,0,9,1},
 		{0,0,9,5,0,1,4,6,2},
@@ -45,8 +57,19 @@ public class SudokuGame extends JFrame implements ActionListener{
 		{0,0,4,1,5,0,6,0,9},
 		{9,0,0,8,7,4,2,1,0},
 	};
-	//Hard Board
-	private int[][] hardTable = new int[][]{
+	private int[][] mediumTable2 = new int[][]{
+		{0,5,7,6,1,3,0,8,4},
+		{4,8,3,2,5,7,1,0,6},
+		{6,1,2,8,0,9,5,3,7},
+		{0,7,8,3,6,4,9,0,2},
+		{5,2,4,0,7,1,3,6,8},
+		{3,6,9,5,0,8,7,0,1},
+		{0,4,5,0,9,2,6,1,3},
+		{2,9,1,4,3,6,0,7,5},
+		{0,3,0,1,8,5,0,2,9},
+	};
+	//Hard Boards
+	private int[][] hardTable1 = new int[][]{
 		{1,0,6,0,0,2,3,0,0},
 		{0,5,0,0,0,6,0,9,1},
 		{0,0,9,5,0,1,4,6,2},
@@ -56,17 +79,29 @@ public class SudokuGame extends JFrame implements ActionListener{
 		{0,0,0,2,6,0,5,4,0},
 		{0,0,4,1,5,0,6,0,9},
 		{9,0,0,8,7,4,2,1,0},
+	};
+	private int[][] hardTable2 = new int[][]{
+		{9,5,0,6,1,0,2,8,4},
+		{4,8,0,2,5,0,1,9,6},
+		{6,1,2,8,4,9,5,0,0},
+		{1,0,8,0,6,4,9,5,2},
+		{5,2,4,9,0,1,0,6,8},
+		{0,6,9,5,2,8,0,4,1},
+		{8,4,5,0,9,2,6,1,0},
+		{2,9,1,4,0,6,8,0,5},
+		{0,0,6,1,8,5,4,2,9},
 	};
 	
 	String[] difficulties = {"easy","medium","hard"};
 	JComboBox diffSelection = new JComboBox(difficulties);
 	JLabel text = new JLabel();
+	Random r = new Random();
 	
 	//Constructor for when I make a JComboBox
 	public SudokuGame() {
 		setLayout(new FlowLayout());
-		setSize(300,200);
-		setTitle("Sudoku Game Difficulty Chooser");
+		setSize(300,130);
+		setTitle("Choose Difficulty");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		diffSelection.setSelectedIndex(1);			//first option is auto selected
@@ -86,25 +121,25 @@ public class SudokuGame extends JFrame implements ActionListener{
 			switch(selected) {
 			case "easy": 
 				text.setText("Selected Difficulty: EASY");
-				makeBoard(easyTable);
+				makeRandBoard("easy");
 				break;
 			case "medium":
 				text.setText("Selected Difficulty: MEDIUM");
-				makeBoard(mediumTable);
+				makeRandBoard("medium");
 				break;
 			case "hard":
 				text.setText("Selected Difficulty: HARD");
-				makeBoard(hardTable);
+				makeRandBoard("hard");
 				break;
 			default: 
 				text.setText("Error");
 			}
-			HaveUserSolveIt();
+			haveUserSolveIt();
 		}	
 	}
 	
 	//Has the user fill out the table with the option to quit whenever
-	private void HaveUserSolveIt() {
+	private void haveUserSolveIt() {
 		String input;
 		while(true) {
 			input = JOptionPane.showInputDialog("Type quit to exit, or just hit enter to continue");
@@ -206,7 +241,7 @@ public class SudokuGame extends JFrame implements ActionListener{
 		}
 		return true;
 	}
-	
+
 	//Checks isSolved in order to end the program
 	private void endProgram() {
 		if(!isSolved()) {
@@ -216,7 +251,21 @@ public class SudokuGame extends JFrame implements ActionListener{
 		}
 	}
 
-	//Makes a specified board
+	//Chooses a random board to make of specified difficulty
+	private void makeRandBoard(String difficulty) {
+		switch(r.nextInt(2)) {
+		case 0:
+			if(difficulty.equals("easy")) makeBoard(easyTable1);
+			if(difficulty.equals("medium")) makeBoard(mediumTable1);
+			if(difficulty.equals("hard")) makeBoard(hardTable1);
+		case 1:
+			if(difficulty.equals("easy")) makeBoard(easyTable2);
+			if(difficulty.equals("medium")) makeBoard(mediumTable2);
+			if(difficulty.equals("hard")) makeBoard(hardTable2);
+		}
+	}
+	
+	//Makes a board
 	private void makeBoard(int[][] table) {
 		for(int row = 0; row < 9; row++) {
 			for(int col = 0; col < 9; col++) {
@@ -240,6 +289,6 @@ public class SudokuGame extends JFrame implements ActionListener{
 	//main
 	public static void main(String[] args) {
 		SudokuGame diffChooser = new SudokuGame();
-		diffChooser.setVisible(true);
+		diffChooser.setVisible(true);	
 	}
 }
