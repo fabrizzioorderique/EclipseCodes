c62b5e8f7cea980899898ed508e3ac087c7021c2
fabrizzioorderique <fabrizzioorderique@gmail.com>
2019-05-05 06:55:40 -0400

    minesweep

---
+++ RandomProjects/src/minesweeper/Mines.java
@@ -0,0 +1,40 @@
+package minesweeper;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class Mines extends JFrame {
+
+    private JLabel statusbar;
+    
+    public Mines() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+
+        statusbar = new JLabel("");
+        add(statusbar, BorderLayout.SOUTH);
+
+        add(new Board(statusbar));        
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Minesweeper");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            Mines ex = new Mines();
+            ex.setVisible(true);
+        });
+    }
+}
