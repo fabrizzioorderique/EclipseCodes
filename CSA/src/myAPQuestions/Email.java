33caa965c740f3a848d41f9515582b6e550fd89e
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-01-17 13:17:08 -0500

    testing csa code

---
+++ CSA/src/myAPQuestions/Email.java
@@ -0,0 +1,33 @@
+package myAPQuestions;
+
+public class Email
+
+{
+
+private String username;
+
+public Email(String u)
+
+{
+
+username = u;
+
+}
+
+public void printThis()
+
+{
+
+System.out.println(this);
+
+}
+
+public String toString()
+
+{
+
+return username + "@example.com";
+
+}
+
+}
---
+++ CSA/src/myAPQuestions/TestingCode.java
@@ -0,0 +1,13 @@
+package myAPQuestions;
+
+public class TestingCode {
+	
+	public static void main(String[] args) {
+		Email e = new Email("default");
+
+		e.printThis();
+	}
+}
+
+
+
