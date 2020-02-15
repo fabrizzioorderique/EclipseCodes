bf55ad5428a9578751a132718d3a5dc4bb96d427
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-02-14 19:51:07 -0500

    working frequencies for sort class

--- CSA/src/algorithms/Sort.java
+++ CSA/src/algorithms/Sort.java
@@ -4,7 +4,22 @@ import java.io.File;
 import java.util.*;
 import javax.swing.JFileChooser;
 /*
- * Sorts int arrays in decending order!
+ * @author Piero Orderique
+ * @date 2/14/2020
+ * @class CSA 2
+ * 
+ * Description:
+ * 	-Sort class contains three different sorting algorithms:
+ * 		-Selection Sort
+ * 		-Insertion Sort
+ * 		-Merge Sort
+ * 
+ * 	-Program is intended to...
+ * 		-Read in a file of integers from 1 - 10
+ * 		-Convert file into an int array 
+ * 		-Sort array in ascending order using all 3 algorithms
+ * 		-Display number of steps taken by each sorting method 
+ * 		-Display frequency of each integer in given array
  */
 //"C:\\Users\\fabri\\OneDrive\\Documents\\DasText\\random10kIntegers.txt"
 
@@ -12,6 +27,7 @@ public class Sort {
 	private static int mergeStep = 0;
 	private static int[] integers = new int[10000]; // holds all integers
 	private static int[][] temps = new int[3][10000];
+	private static int[] frequencies = new int[10];	//holds frequencies
 
 	public static void main(String[] args) {
 		JFileChooser fc = new JFileChooser();
@@ -21,30 +37,15 @@ public class Sort {
 			int result = fc.showOpenDialog(fc);
 			if (result == JFileChooser.APPROVE_OPTION) {
 				File file = fc.getSelectedFile();
-				try {
-					createData(file);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				try {createData(file);} 
+				catch (Exception e) {e.printStackTrace();}
 				String path = file.getAbsolutePath();
 				System.out.println(path + " is now loaded in.");
 				System.out.println();
 			}
 			setTemps(); // sets temps to original unsorted array
-			/*System.out.println("Array Input: " + Arrays.toString(integers));
-			System.out.println();
-			System.out.println("Sort selected: Selection Sort");
-			System.out.println(Arrays.toString(selectionSort(temps[0])));
-			System.out.println();
-			System.out.println("Sort selected: Insertion Sort");
-			System.out.println(Arrays.toString(selectionSort(temps[1])));
-			System.out.println(); */
-			System.out.println("Sort selected: Selection Sort");
-			selectionSort(temps[0]);
-			System.out.println("Sort selected: Insertion Sort");
-			insertionSort(temps[1]);
-			System.out.println("Sort selected: Merge Sort");
-			mergeSort(temps[2]);
+			displaySteps();
+			showFrequencies();
 		}else {System.out.println("Program Terminated.");}
 	}
 
@@ -71,38 +72,35 @@ public class Sort {
 
 	// prints arrays - for testing
 	public static void printArray(int[] array) {
-		for (int i : array) {
-			System.out.println(i);
-		}
+		for (int i : array) {System.out.println(i);}
 	}
 
 	public static void printArray(int[][] array) {
 		for (int[] i : array) {
-			for (int j : i) {
-				System.out.print(j + " ");
-			}
+			for (int j : i) {System.out.print(j + " ");}
 			System.out.println();
 		}
 	}
-
-	// method to do whichever sort is chosen
-	public static void sortArray(String sort, int[] arr) {
-		int[] testArr = arr;
-		System.out.println("Array Input: " + Arrays.toString(testArr));
-		System.out.println("Sort selected: "+sort);
-		switch (sort) {
-		case "selection":
-			selectionSort(testArr);
-		case "insertion":
-			insertionSort(testArr);
-		case "merge":
-			mergeSort(testArr);
-		}
-		System.out.println("SORTED LIST: " + Arrays.toString(testArr));
-		System.out.println();
+	
+	//displays the amount of steps each sorting algorthm takes
+	public static void displaySteps() {
+		System.out.println("Sort selected: Selection Sort");
+		selectionSort(temps[0]);
+		System.out.println("Sort selected: Insertion Sort");
+		insertionSort(temps[1]);
+		System.out.println("Sort selected: Merge Sort");
+		mergeSort(temps[2]);
 	}
-
-	/************************ SORT METHODS **************************/
+	
+	//displays the frequencies of each integer in a list
+	public static void showFrequencies() {
+		for(int i : integers) {frequencies[i-1]++;}
+		for(int i = 0; i < frequencies.length;i++) {
+			System.out.println("Frequency of "+(i+1)+": "+frequencies[i]);
+		}
+	}
+	
+	/*************************** SORTING METHODS *******************************/
 	
 	// SELECTION SORT -- n(n - 1) / 2 comparisons no matter what
 	public static int[] selectionSort(int[] elements) {
@@ -114,7 +112,6 @@ public class Sort {
 					minIndex = k;
 				}
 				selectionSteps++;
-				// System.out.println("Step "+selectionSteps+ ": "+Arrays.toString(elements));
 			}
 			int temp = elements[j];
 			elements[j] = elements[minIndex];
@@ -139,14 +136,13 @@ public class Sort {
 			}
 			steps++;
 			elements[possibleIndex] = temp;
-			// System.out.println("Step "+steps+ ": "+Arrays.toString(elements));
 		}
 		System.out.println("Number of steps taken: " + (steps + whileCount));
 		System.out.println();
 		return elements;
 	}
 
-	/****** MERGE SORT *******/
+	//MERGE SORT
 	public static int[] mergeSort(int[] elements) {
 		int n = elements.length;
 		int[] temp = new int[n];
@@ -180,7 +176,6 @@ public class Sort {
 				j++;
 			}
 			k++;
-			// System.out.println("Step "+mergeStep+ ": "+Arrays.toString(elements));
 		}
 		while (i <= mid) {
 			temp[k] = elements[i];
