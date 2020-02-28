f1a2cf1c2062c123d6eb15eca4de996171970ade
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-02-28 12:11:37 -0500

    started ProjectProgramming program

--- CSA/src/myAPQuestions/A.java
+++ CSA/src/myAPQuestions/A.java
@@ -1,6 +1,38 @@
 package myAPQuestions;
+
+import java.util.ArrayList;
+
 public class A{
-	public void show() {
-		System.out.println("A");
+	public static int mystery(ArrayList<Integer> numList)
+
+	{
+
+	if (numList.size() == 0)
+
+	{
+
+	return 0;
+
 	}
-}
+
+	else
+
+	{
+
+	int val = numList.remove(0);
+
+	return val + mystery(numList);
+
+	}
+
+	}
+	public static void main(String[] args) {
+		ArrayList<Integer> ints = new ArrayList<>();
+		ints.add(4);
+		ints.add(52);
+		ints.add(32);
+		ints.add(12);
+		System.out.println(mystery(ints));
+		
+	}
+} 
